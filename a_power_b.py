a557b1a192301547df98e28c6f090b253154cc3b
angelasofiaremolinagutierrez <informatica@bga.upb>
2019-03-29 11:02:12 -0500

    Al escribir cero, para

--- a_power_b.py
+++ a_power_b.py
@@ -1,9 +1,18 @@
-a=int(input("Ingrese el número base: "))
-b=int(input("Ingrese el número exponente: "))
 def a_power_b():
+
     prod_acum = 1
     for x in range (1,b+1):
         prod_acum = prod_acum * a
     print(prod_acum)
 
+
+a=int(input("Ingrese el número base: "))
+b=int(input("Ingrese el número exponente: "))
 a_power_b()
+
+while a!=0:
+    a=int(input("Ingrese el número base: "))
+    b=int(input("Ingrese el número exponente: "))
+    a_power_b()
+
+print("Carácter no válido")
