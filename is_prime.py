348f8db82576f64e8c73df74d9330b03a4b88e41
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2019-03-29 16:40:53 -0500

    imprima 1,0, ó -1

--- is_prime.py
+++ is_prime.py
@@ -1,15 +1,21 @@
 def is_prime():
-  div = 0
-  for x in range (1,n+1):
-    if (n%x) == 0:
-      div = div + 1
+    div = 0
+    for x in range (1,n+1):
+        if (n%x) == 0:
+          div = div + 1
 
-  if div>2:
-    print("Is NOT a prime number")
-  else:
-    print("Is a prime number")
-#cero(0) = no primo
-#uno(1) = primo
-#-uno(-1) = otro
-n = int(input("Enter a number: "))
-is_prime()
+    primo = 1
+    no_primo = 0
+    otro = -1
+    if div<=2 and div>0:
+        print(primo)
+    elif div>2:
+        print(no_primo)
+    else:
+        print(otro)
+
+try:
+    n = int(input("Ingresa un número: "))
+    is_prime()
+except ValueError:
+    print ("Carácter no válido. Intenta otra vez")
